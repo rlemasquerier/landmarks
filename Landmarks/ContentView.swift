1e2867986d9fcc6c132d6ad47f6eb5928d4ff7a2
Rodolphe Lemasquerier <rodolphe.lemasquerier@gmail.com>
2019-11-09 17:56:02 +0100

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Rodolphe Lemasquerier on 09/11/2019.
+//  Copyright © 2019 Rodolphe Lemasquerier. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
